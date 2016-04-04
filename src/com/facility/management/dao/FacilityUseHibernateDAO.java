7103e744903df55ff8d8b6c7a642ddad86f66894
Tara Raj <tararaj21@gmail.com>
2016-04-04 15:49:32 -0500

    updated InspectionsHibernateDAO

--- src/com/facility/management/dao/FacilityUseHibernateDAO.java
+++ src/com/facility/management/dao/FacilityUseHibernateDAO.java
@@ -2,16 +2,26 @@ package com.facility.management.dao;
 
 import org.hibernate.Session;
 
+import com.facility.management.model.facility.Facility;
 import com.facility.management.model.facility_use.Facility_use;
 import com.facility.management.model.service.FacilityUseService;
 
 public class FacilityUseHibernateDAO {
-	public void addFacilityUse(Facility_use facUse) {
+	public void assignFacilityToUse(Facility_use facUse) {
 		System.out.println("*************** Adding facilityUse information in DB with ID ...  " 
 				+ facUse.getUseId());
 		Session session = HibernatePGSQLHelper.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		session.save(facUse);
 		session.getTransaction().commit();
+	}
+	
+	public void vacateFacility(Facility_use fac) {
+		System.out.println("*************** Deleteing facility from DB with ID ...  " + fac.getFacilityId());
+		Session session = HibernatePGSQLHelper.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.delete(fac);
+		session.getTransaction().commit();
+		
 	}
 }
--- src/com/facility/management/dao/InspectionsHibernateDAO.java
+++ src/com/facility/management/dao/InspectionsHibernateDAO.java
@@ -1,7 +1,12 @@
 package com.facility.management.dao;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 
+import com.facility.management.model.facility.Facility;
 import com.facility.management.model.inspections.Inspections;
 import com.facility.management.model.service.InspectionService;
 
@@ -13,5 +18,28 @@ public class InspectionsHibernateDAO {
 		session.beginTransaction();
 		session.save(inspect);
 		session.getTransaction().commit();
+	}
+	
+	public List<Facility> listFacilities(){
+		try {
+		System.out.println("*************** querying facilities ...  ");
+		Session session = HibernatePGSQLHelper.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+        
+        Query listInspectionsQuery = session.createQuery("From InspectionsImpl");	
+		
+		List ins = listInspectionsQuery.list(); 
+		
+		System.out.println("*************** Retrieve Query is ....>>\n" + ins.size()); 
+		
+		session.getTransaction().commit();
+		return ins;
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Collections.emptyList();
+
+		
 	}
 }
